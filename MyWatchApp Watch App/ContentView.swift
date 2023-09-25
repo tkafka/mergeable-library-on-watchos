54da460bf29eb3b8be98c12d4933f51231556c10
Tomáš Kafka <tk@tomaskafka.com>
2023-09-25 13:57:46 +0200

    trz to add fw

--- MyWatchApp Watch App/ContentView.swift
+++ MyWatchApp Watch App/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MyKit
 
 struct ContentView: View {
     var body: some View {
@@ -13,7 +14,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+					Text("Hello, \(MyEnum.one.str)!")
         }
         .padding()
     }
